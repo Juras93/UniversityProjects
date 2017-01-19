61d588ca308dff1622c7e1e3ffbd892f02a8a842
Juras93 <jerzy.gawor@gmail.com>
2017-01-19 20:26:31 +0100

    Prepare basic view for recipe site with css styles

---
+++ CulinaryRecipesPage/Pages/index.php
@@ -0,0 +1,28 @@
+<html>
+    <link rel="stylesheet" href="../Styles/main.css">
+    <head>
+    </head>
+    <body>
+        <div class="site">
+            <div class="main">
+                <div class="greetings">
+                    <h2>Witaj na Przepisy.org.pl</h2>
+                </div>
+                <div class="recipes">
+                    <!--
+                        Here, will be product list generated from database..
+                        Should works as scrollable div
+                    -->    
+                </div>
+                <h1>Takie sobie cos</h1>
+            </div>
+            <div class="newRecipe">
+                <form id="recipeForm">
+                    <input class="formControl" type="text" placeholder="Nazwa potrawy"><br />
+                    <textarea class="formControl" type="text" form="recipeForm">Przepis</textarea><br />
+                    <input type="submit">
+                </form>
+            </div>
+        </div>
+    </body>
+</html>
