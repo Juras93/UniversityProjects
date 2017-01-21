1443c1eebff4e19b292ca4a81046ecd69fbc0179
Juras93 <jerzy.gawor@gmail.com>
2017-01-21 08:23:25 +0100

    -Add new logo -Add navigation links

--- CulinaryRecipesPage/Pages/index.php
+++ CulinaryRecipesPage/Pages/index.php
@@ -4,10 +4,15 @@
     </head>
     <body>
         <div class="site">
+            <div class="logo">
+                <img src="../Graphics/przepisyLogo.png"></img>
+            </div>
+            <div class="navigationBar">
+                <a href="index.php">Strona Glowna</a>
+                <a href="loginPage.html">Zaloguj</a>
+                <a href="loginPage.html">Zarejestruj</a>
+            </div>
             <div class="main">
-                <div class="greetings">
-                    <img src="../Graphics/Logo.png"></img>
-                </div>
                 <div class="recipes">
                     <!--
                         Here, will be product list generated from database..
@@ -15,14 +20,13 @@
                     -->    
                 </div>
                 <h1>Takie sobie cos</h1>
-            </div>
-            <div class="newRecipe">
                 <form id="recipeForm">
                     <input class="formControl" type="text" placeholder="Nazwa potrawy"><br />
                     <textarea class="formControl" type="text" form="recipeForm">Przepis</textarea><br />
                     <button type="submit">Dodaj przepis</button>
                 </form>
             </div>
+            
         </div>
     </body>
 </html>
