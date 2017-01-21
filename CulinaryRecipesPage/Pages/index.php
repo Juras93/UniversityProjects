67d2c05d9091f54e08ea33f2d5fbdb29fc94fe5a
Juras93 <jerzy.gawor@gmail.com>
2017-01-21 16:30:27 +0100

    - remove unuseful site - move logging functionality to index - change css styles

--- CulinaryRecipesPage/Pages/index.php
+++ CulinaryRecipesPage/Pages/index.php
@@ -9,24 +9,37 @@
             </div>
             <div class="navigationBar">
                 <a href="index.php">Strona Glowna</a>
-                <a href="loginPage.html">Zaloguj</a>
-                <a href="loginPage.html">Zarejestruj</a>
             </div>
             <div class="main">
-                <div class="recipes">
+                <div class="leftDiv">
+                    <div class="Authorized">
+                        <div class="loggedAs">
+
+                        </div>
+                        <div class="addRecipe">
+                            <form id="recipeForm">
+                                <input class="formControl" type="text" placeholder="Nazwa potrawy"><br />
+                                <textarea class="formControlDesc" type="text" form="recipeForm">Przepis</textarea><br />
+                                <button type="submit">Dodaj przepis</button>
+                            </form>
+                        </div>
+                    </div>
+                    <div class="noAuthorized">
+                        <form class="loginForm">
+                            <input class="loginControl" type="text" placeholder="Login"><br />
+                            <input class="loginControl" type="password" placeholder="Password"><br />
+                            <button type="submit">Zaloguj</button>
+                            <button>Nie masz konta?</button>
+                        </form>
+                    </div>
+                </div>
+                <div class="rightDiv">
                     <!--
                         Here, will be product list generated from database..
                         Should works as scrollable div
                     -->    
                 </div>
-                <h1>Takie sobie cos</h1>
-                <form id="recipeForm">
-                    <input class="formControl" type="text" placeholder="Nazwa potrawy"><br />
-                    <textarea class="formControl" type="text" form="recipeForm">Przepis</textarea><br />
-                    <button type="submit">Dodaj przepis</button>
-                </form>
             </div>
-            
         </div>
     </body>
 </html>
