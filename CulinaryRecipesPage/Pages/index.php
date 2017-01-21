a2221f8125e048f6bb996160696811bfcf0295f1
Jerzy Gawor <jerzy.gawor@gmail.com>
2017-01-21 18:45:40 +0100

    Update index view and add bootstrap to project

--- CulinaryRecipesPage/Pages/index.php
+++ CulinaryRecipesPage/Pages/index.php
@@ -1,45 +1,53 @@
 <html>
     <link rel="stylesheet" href="../Styles/main.css">
+	<link href="../Styles/css/bootstrap.min.css" rel="stylesheet">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+	<script src="../Scripts/bootstrap/bootstrap.min.js"></script>
+
     <head>
     </head>
     <body>
-        <div class="site">
-            <div class="logo">
-                <img src="../Graphics/przepisyWysmienite.png"></img>
-            </div>
-            <div class="navigationBar">
-                <a href="index.php">Strona Glowna</a>
-                <a href="index.php">O nas</a>
-            </div>
-            <div class="main">
-                <div class="leftDiv">
-                    <div class="Authorized">
-                        <div class="loggedAs">
-                            Zalogowany jako <span class="loginName">Guest</span>
-                        </div>
-                        <div class="addRecipe">
-                            <form id="recipeForm">
-                                <input class="formControl" type="text" placeholder="Nazwa potrawy"><br />
-                                <textarea class="formControlDesc" type="text" form="recipeForm">Przepis</textarea><br />
-                                <button type="submit">Dodaj przepis</button>
-                            </form>
-                        </div>
-                    </div>
-                    <div class="noAuthorized">
-                        <form class="loginForm">
-                            <input class="loginControl" type="text" placeholder="Login"><br />
-                            <input class="loginControl" type="password" placeholder="Password"><br />
-                            <button type="submit">Zaloguj</button>
-                            <button>Nie masz konta?</button>
-                        </form>
-                    </div>
-                </div>
-                <div class="rightDiv">
-                    <!--
-                        Here, will be product list generated from database..
-                        Should works as scrollable div
-                    -->    
-                </div>
+        <div class="container-fluid">
+			<div class="row">
+				<div class="col-md-3 col-s-12 col-xs-12 logo">
+					<img src="../Graphics/przepisyWysmienite.png"></img>
+				</div>
+				<div class="col-md-9 col-s-12 col-xs-12 navigationBar">
+					<a href="index.php">Strona Glowna</a>
+					<a href="">O nas</a>
+					<a href="">Zakupy</a>
+					<a href="">Kontakt</a>
+					<a href="">!Praca!</a>
+				</div>
+			</div>
+            <div class="row">
+				<div class="col-md-3 col-s-12">
+					<!--<div class="Authorized">
+						<div class="loggedAs">
+							Zalogowany jako <span class="loginName">Guest</span>
+						</div>
+						<div class="addRecipe">
+							<form id="recipeForm">
+								<div class="form-group">
+									<label class="form-check-label">Nazwa potrawy:</label><input class="formControl form-control" type="text" placeholder="Wpisz nazwe potrawy"><br />
+									<label class="form-check-label">Przepis:</label><textarea class="formControlDesc form-control" type="text" form="recipeForm"></textarea><br />
+									<button class="btn btn-primary" type="submit">Dodaj przepis</button>
+								</div>
+							</form>
+						</div>
+					</div>-->
+					<div class="noAuthorized">
+						<form class="loginForm form-group">
+							<label class="form-check-label">Login:</label><input class="form-control" type="text" placeholder="Login"><br />
+							<label class="form-check-label">Haslo:</label><input class="form-control" type="password" placeholder="Password"><br />
+							<button class="btn btn-primary" type="submit">Zaloguj</button>
+							<button class="btn ">Nie masz konta?</button>
+						</form>
+					</div>
+				</div>
+				<div class="col-md-9 col-s-12">
+					  Lallalal
+				</div>
             </div>
         </div>
     </body>
