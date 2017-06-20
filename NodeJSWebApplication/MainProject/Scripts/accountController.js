6781a5cc8dd688cc04a744571bbe440bf5427052
Juras93 <jerzy.gawor@gmail.com>
2017-06-20 21:40:37 +0200

    add component for dialogs. Thanks to it, i have possibility to use controller scope there.

--- NodeJSWebApplication/MainProject/Scripts/accountController.js
+++ NodeJSWebApplication/MainProject/Scripts/accountController.js
@@ -1,17 +1,61 @@
-var angularApp = angular.module('application', ['ngCookies']);
-angularApp.controller('accountController', ['$scope', '$http', '$cookies',
-function($scope, $http, $cookies){
-    $scope.isUserSignIn = false;
+var angularApp = angular.module('application', ['ngAnimate', 'ngSanitize', 'ngCookies', 'ui.bootstrap']);
 
-    $scope.login = () => {
-        
+angularApp.controller('ModalInstanceCtrl', function ($uibModalInstance) {
+  var $accountCtrl = this;
+
+  $accountCtrl.ok = function () {
+
+  };
+
+  $accountCtrl.cancel = function () {
+    $uibModalInstance.dismiss('cancel');
+  };
+});
+
+angularApp.component('loginModalComponent', {
+    templateUrl: 'loginModal.html',
+    bindings: {
+        resolve: '<',
+        close: '&',
+        dismiss: '&'
+    },
+    controller: () => {
+        var $accountCtrl = this;
+
+        $accountCtrl.$onInit = () => {
+
+        };
+
+        $accountCtrl.okButton = () => {
+            console.log('Liiiii');
+            //HTTP request about login
+        };
+
+        $accountCtrl.closeButton = () => {
+            $accountCtrl.close();
+        }
+    }
+});
+
+
+angularApp.controller('accountController', ['$scope', '$http', '$cookies', '$uibModal',
+function($scope, $http, $cookies, $uibModal){
+    var $accountCtrl = this;
+
+    $accountCtrl.isUserSignIn = false;
+
+    $accountCtrl.loginModalOpen = () => {
+        var modalInstance = $uibModal.open({
+            animation: true,
+            component: 'loginModalComponent'
+        });
     };
 
-    $scope.signUp = () => {
+    $accountCtrl.signUp = () => {
 
     };
 
-    $scope.logout = () => {
+    $accountCtrl.logout = () => {
 
     }
 }]);
